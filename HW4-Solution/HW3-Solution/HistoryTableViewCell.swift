8ed5fea608358f517bd8f7c642cf3e28b6df6d0d
fountaig <33142528+fountaig@users.noreply.github.com>
2019-11-01 09:55:38 -0400

    Add files via upload
    We done bois

---
+++ HW4-Solution/HW3-Solution/HistoryTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  HistoryTableViewCell.swift
+//  HW3-Solution
+//
+//  Created by Xcode User on 11/1/19.
+//  Copyright © 2019 Jonathan Engelsma. All rights reserved.
+//
+
+import UIKit
+
+class HistoryTableViewCell: UITableViewCell {
+    @IBOutlet weak var thumbnail: UIImageView!
+    @IBOutlet weak var timestampLabel: UILabel!
+    @IBOutlet weak var conversionLabel: UILabel!
+}
